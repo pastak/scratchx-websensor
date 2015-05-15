dbbe3cb7910469c879132d718bfcdd4c28d8e42d
pastak <pasta0915@gmail.com>
2015-05-15 21:36:23 +0900

    acceleration

--- README.md
+++ README.md
@@ -10,20 +10,26 @@ Extension script URL is `http://pastak.github.io/scratchx-websensor/script.js`
 
 **Caution**
 
-This extension only run on Firefox with MacOSX or Android.
+Some blocks only run on Firefox with MacOSX or Android.
 
 Google Chrome and Firefox for other OS don't support these API.
 
 ## Blocks
 
-### Light sensor (Int)
+### Light sensor (Int) **Only Firefox**
 
 ![](https://i.gyazo.com/608840a12f786d529e00051fc73901b6.png)
 
 This block return value get with front camera.( unit is lux )
 
-### Battery Level (Int)
+### Battery Level (Int) **Only Firefox**
 
 ![](https://i.gyazo.com/5a6e87a1436c1a6e1e894bad2d1c7a93.png)
 
 This block return battery level with percentage.
+
+### Acceleration (Float)
+
+![](https://i.gyazo.com/96d23dd07f46a56dfdc68a2d9a1dcbca.png)
+
+This block return acceleration includes the effect of gravity.
--- script.js
+++ script.js
@@ -11,9 +11,19 @@
       }
     })
 
+    $(window).on('devicemotion', function(event){
+      ext.getAcceleration = function(direction){
+        if(!direction){
+          return 0;
+        }
+        return event.originalEvent.accelerationIncludingGravity[direction]
+      }
+    })
+
     // blockが呼び出された時に呼ばれる関数を登録する。
     // 下にあるdescriptorでブロックと関数のひも付けを行っている。
     ext.getLightSensor = function() {};
+    ext.getAcceleration = function() {};
     ext.getBatteryLevel = function() {
       var battery = navigator.battery;
       if(!battery){
@@ -27,9 +37,13 @@
     var descriptor = {
         blocks: [
             ['r', 'Light sensor', 'getLightSensor',null],
-            ['r', 'Battery', 'getBatteryLevel', null],
-        ]
+            ['r', 'Battery', 'getBatteryLevel',null],
+            ['r', 'Acceleration %m.acceleration', 'getAcceleration', 'x'],
+        ],
+        menus: {
+          acceleration: ['x', 'y', 'z']
+        }
     };
 
-    ScratchExtensions.register('WebAPI', descriptor, ext);
+    ScratchExtensions.register('WebAPI Sensors', descriptor, ext);
 })({});
