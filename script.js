6706a73a90346204769260615a0437321e586275
pastak <pasta0915@gmail.com>
2015-05-15 14:37:05 +0900

    get Battery Level

--- script.js
+++ script.js
@@ -14,11 +14,20 @@
     // blockが呼び出された時に呼ばれる関数を登録する。
     // 下にあるdescriptorでブロックと関数のひも付けを行っている。
     ext.getLightSensor = function() {};
+    ext.getBatteryLevel = function() {
+      var battery = navigator.battery;
+      if(!battery){
+        return 0;
+      }else{
+        return Math.round(battery.level * 100)
+      }
+    };
 
     // ブロックと関数のひも付け
     var descriptor = {
         blocks: [
             ['r', 'Light sensor', 'getLightSensor',null],
+            ['r', 'Battery', 'getBatteryLevel', null],
         ]
     };
 
